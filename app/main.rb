0313f8e1ea78219f63f63ba7596ce103cd71fc4f
Brett Chalupa <brettchalupa@gmail.com>
2022-12-31 10:12:53 -0500

    feat: moveable player square

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,4 @@
 FPS = 60
-TRUE_BLACK = { r: 0, g: 0, b: 0 }
-WHITE = { r: 255, g: 255, b: 255 }
 ALIGN_LEFT = 0
 ALIGN_CENTER = 1
 ALIGN_RIGHT = 2
@@ -9,6 +7,9 @@ BLEND_ALPHA = 1
 BLEND_ADDITIVE = 2
 BLEND_MODULO = 3
 BLEND_MULTIPLY = 4
+
+TRUE_BLACK = { r: 0, g: 0, b: 0 }
+WHITE = { r: 255, g: 255, b: 255 }
 
 # Access in code with `SPATHS[:my_sprite]`
 # Replace with your sprites!
@@ -25,7 +26,37 @@ def tick(args)
 
   args.outputs.background_color = TRUE_BLACK.values
 
+  args.state.player ||= {
+    x: args.grid.w / 2,
+    y: args.grid.h / 2,
+    w: 32,
+    h: 32,
+    speed: 6,
+  }.merge(WHITE)
+
+  tick_player(args, args.state.player)
+
+  args.outputs.solids << args.state.player
+
   debug_tick(args)
+end
+
+def tick_player(args, player)
+  if args.inputs.down
+    player.y -= player.speed
+  end
+
+  if args.inputs.up
+    player.y += player.speed
+  end
+
+  if args.inputs.left
+    player.x -= player.speed
+  end
+
+  if args.inputs.right
+    player.x += player.speed
+  end
 end
 
 # The version of your game defined in `metadata/game_metadata.txt`
@@ -34,7 +65,7 @@ def version
 end
 
 def debug?
-  !$gtk.production
+  @debug ||= !$gtk.production
 end
 
 def debug_tick(args)
