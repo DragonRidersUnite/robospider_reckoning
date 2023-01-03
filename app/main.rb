20be534719e9a7fe30eddfc04bd4b72121d6ea5b
Brett Chalupa <brettchalupa@gmail.com>
2023-01-03 09:42:35 -0500

    chore: make generic tick_menu method
    for using with the other various scenes

--- app/main.rb
+++ app/main.rb
@@ -74,70 +74,11 @@ def tick_main_menu(args)
     }
   end
 
-  args.state.main_menu.current_option_i ||= 0
-  args.state.hold_delay ||= 0
+  tick_menu(args, :main_menu, options)
 
-  labels = []
-
-  options.each.with_index do |option, i|
-    label = label(
-      option[:text],
-      x: args.grid.w / 2,
-      y: 360 + (options.length - i * 52),
-      align: ALIGN_CENTER,
-      size: SIZE_MD
-    )
-    label_size = args.gtk.calcstringbox(label.text, label.size_enum)
-    labels << label
-    if args.state.main_menu.current_option_i == i
-      args.outputs.solids << {
-        x: label.x - (label_size[0] / 1.4) - 24 + (Math.sin(args.state.tick_count / 8) * 4),
-        y: label.y - 22,
-        w: 16,
-        h: 16,
-      }.merge(WHITE)
-    end
-  end
-
-  labels << label(title,
-    x: args.grid.w / 2, y: args.grid.top - 100,
+  args.outputs.labels << label(
+    title, x: args.grid.w / 2, y: args.grid.top - 100,
     size: SIZE_LG, align: ALIGN_CENTER)
-
-  args.outputs.labels << labels
-
-  move = nil
-  if args.inputs.down
-    move = :down
-  elsif args.inputs.up
-    move = :up
-  else
-    args.state.hold_delay = 0
-  end
-
-  if move
-    args.state.hold_delay -= 1
-
-    if args.state.hold_delay <= 0
-      index = args.state.main_menu.current_option_i
-      if move == :up
-        index -= 1
-      else
-        index += 1
-      end
-
-      if index < 0
-        index = options.length - 1
-      elsif index > options.length - 1
-        index = 0
-      end
-      args.state.main_menu.current_option_i = index
-      args.state.hold_delay = 10
-    end
-  end
-
-  if primary_down?(args.inputs)
-    options[args.state.main_menu.current_option_i][:on_select].call(args)
-  end
 end
 
 def switch_scene(args, scene, reset: false)
@@ -563,4 +504,77 @@ def debug_label(args, x, y, text)
   return unless args.state.render_debug_details
 
   args.outputs.debug << { x: x, y: y, text: text }.merge(WHITE).label!
+end
+
+# Updates and renders a list of options that get passed through.
+#
+# +options+ data structure:
+# [
+#   {
+#     text: "some string",
+#     on_select: -> (args) { "do some stuff in this lambda" }
+#   }
+# ]
+def tick_menu(args, state_key, options)
+  args.state.send(state_key).current_option_i ||= 0
+  args.state.send(state_key).hold_delay ||= 0
+  menu_state = args.state.send(state_key)
+
+  labels = []
+
+  options.each.with_index do |option, i|
+    label = label(
+      option[:text],
+      x: args.grid.w / 2,
+      y: 360 + (options.length - i * 52),
+      align: ALIGN_CENTER,
+      size: SIZE_MD
+    )
+    label_size = args.gtk.calcstringbox(label.text, label.size_enum)
+    labels << label
+    if menu_state.current_option_i == i
+      args.outputs.solids << {
+        x: label.x - (label_size[0] / 1.4) - 24 + (Math.sin(args.state.tick_count / 8) * 4),
+        y: label.y - 22,
+        w: 16,
+        h: 16,
+      }.merge(WHITE)
+    end
+  end
+
+  args.outputs.labels << labels
+
+  move = nil
+  if args.inputs.down
+    move = :down
+  elsif args.inputs.up
+    move = :up
+  else
+    menu_state.hold_delay = 0
+  end
+
+  if move
+    menu_state.hold_delay -= 1
+
+    if menu_state.hold_delay <= 0
+      index = menu_state.current_option_i
+      if move == :up
+        index -= 1
+      else
+        index += 1
+      end
+
+      if index < 0
+        index = options.length - 1
+      elsif index > options.length - 1
+        index = 0
+      end
+      menu_state.current_option_i = index
+      menu_state.hold_delay = 10
+    end
+  end
+
+  if primary_down?(args.inputs)
+    options[menu_state.current_option_i][:on_select].call(args)
+  end
 end
