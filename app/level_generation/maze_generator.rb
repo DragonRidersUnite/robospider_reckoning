0a64a3a1e8fb21be818188f4ed401ffd5e7a1798
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-01-16 18:10:02 +0900

    Extract MazeGenerator class and focus responsibilities

---
+++ app/level_generation/maze_generator.rb
@@ -0,0 +1,68 @@
+module LevelGeneration
+  class MazeGenerator
+    def initialize(size:)
+      @size = size
+    end
+
+    def generate
+      @grid = initialize_grid
+      current_cell = @grid[0][0]
+      current_cell[:wall] = true
+      current_cell[:visited] = true
+      stack = []
+      stack.push(current_cell)
+
+      until stack.empty?
+        current_cell = stack.last
+        neighbors = get_neighbors(current_cell)
+        unvisited_neighbors = neighbors.reject(&:visited)
+        if unvisited_neighbors.empty?
+          stack.pop
+        else
+          next_cell = unvisited_neighbors.sample
+          mark_cell_in_between_as_wall(current_cell, next_cell)
+          next_cell[:wall] = true
+          next_cell[:visited] = true
+          stack.push(next_cell)
+        end
+      end
+      build_result
+    end
+
+    private
+
+    def initialize_grid
+      Array.new(@size) { |x|
+        Array.new(@size) { |y|
+          { x: x, y: y, wall: false, visited: false }
+        }
+      }
+    end
+
+    def get_neighbors(current_cell)
+      neighbors = []
+      @grid.each do |row|
+        row.each do |cell|
+          x_diff = (cell[:x] - current_cell[:x]).abs
+          y_diff = (cell[:y] - current_cell[:y]).abs
+          neighbors << cell if (x_diff == 2 && y_diff.zero?) || (x_diff.zero? && y_diff == 2)
+        end
+      end
+      neighbors
+    end
+
+    def mark_cell_in_between_as_wall(current_cell, next_cell)
+      x = (current_cell[:x] + next_cell[:x]).idiv 2
+      y = (current_cell[:y] + next_cell[:y]).idiv 2
+      @grid[x][y][:wall] = true
+    end
+
+    def build_result
+      @grid.map do |row|
+        row.map do |cell|
+          cell.slice(:x, :y, :wall)
+        end
+      end
+    end
+  end
+end
