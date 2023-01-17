a800af0cbfe09ff118a784502f42bdf896c0d988
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-01-17 12:18:01 +0900

    Implement vertical wall extraction

--- app/level_generation.rb
+++ app/level_generation.rb
@@ -3,11 +3,27 @@ module LevelGeneration
     def extract_walls(grid)
       walls = []
 
+      grid.each_with_index do |column, x|
+        current_wall = nil
+        column.each_with_index do |cell, y|
+          unless current_wall
+            current_wall = { x: x, y: y, w: 1, h: 1 } if cell[:wall]
+            next
+          end
+
+          if cell[:wall]
+            current_wall[:h] += 1
+            next
+          end
+
+          walls << current_wall
+          current_wall = nil
+        end
+
+        walls << current_wall if current_wall
+      end
+
       walls += [
-        { x: 0, y: 0, w: 1, h: 3 },
-        { x: 1, y: 0, w: 1, h: 2 },
-        { x: 2, y: 2, w: 1, h: 1 },
-        { x: 3, y: 2, w: 1, h: 1 },
         { x: 0, y: 0, w: 2, h: 1 },
         { x: 0, y: 1, w: 2, h: 1 },
         { x: 0, y: 2, w: 1, h: 1 },
