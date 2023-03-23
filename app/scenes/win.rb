ccb1558418fb51dade77f3c89e5787cbb4164b20
Alex Lopez <webodessa@gmail.com>
2023-03-22 22:46:41 -0400

    Show difficulty on win/game_over scenes

--- app/scenes/win.rb
+++ app/scenes/win.rb
@@ -8,12 +8,13 @@ module Scene
 
       labels = []
 
-      labels << label(:win, x: args.grid.w / 2, y: args.grid.top - 200, align: ALIGN_CENTER, size: SIZE_LG, font: FONT_BOLD)
-      labels << label("#{text(:level)}: #{a_s.player.level}", x: args.grid.w / 2, y: args.grid.top - 320, size: SIZE_SM, align: ALIGN_CENTER)
-      labels << label("#{text(:enemies_destroyed)}: #{a_s.enemies_destroyed}", x: args.grid.w / 2, y: args.grid.top - 380, size: SIZE_SM, align: ALIGN_CENTER)
+      labels << label(:win, x: args.grid.w / 2, y: args.grid.top - 180, align: ALIGN_CENTER, size: SIZE_LG, font: FONT_BOLD)
+      labels << label("#{text(:level)}: #{a_s.player.level}", x: args.grid.w / 2, y: args.grid.top - 300, size: SIZE_SM, align: ALIGN_CENTER)
+      labels << label("#{text(:enemies_destroyed)}: #{a_s.enemies_destroyed}", x: args.grid.w / 2, y: args.grid.top - 360, size: SIZE_SM, align: ALIGN_CENTER)
+      labels << label("#{text(:difficulty)}: #{text(args.state.setting.difficulty)}", x: args.grid.w / 2, y: args.grid.top - 420, size: SIZE_SM, align: ALIGN_CENTER)
 
       if (a_s.player.contemplating -= 1) < 0
-        labels << label(:retry, x: args.grid.w / 2, y: args.grid.top - 480, align: ALIGN_CENTER, size: SIZE_SM).merge(a: a_s.tick_count % 155 + 100)
+        labels << label(:retry, x: args.grid.w / 2, y: args.grid.top - 520, align: ALIGN_CENTER, size: SIZE_SM).merge(a: a_s.tick_count % 155 + 100)
 
         if Input.confirm?(args.inputs)
           fireworks = []
